518a38ed419d34ddffac1ee625f0fc87fdcb729d
Michael <ryuuko@eastlink.ca>
2014-10-28 19:36:02 -0300

    update

---
+++ C#/Lab Samples/menus/menus/Form1.Designer.cs
@@ -0,0 +1,340 @@
+﻿namespace menus
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.submitButton = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.infoTab = new System.Windows.Forms.TabPage();
+            this.print = new System.Windows.Forms.Button();
+            this.enfoTab = new System.Windows.Forms.TabPage();
+            this.sendToClass = new System.Windows.Forms.Button();
+            this.formTwoPass = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveTextToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openYourMindToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tab1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tab2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.form2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.steupToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fontToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.colorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.printForm1 = new Microsoft.VisualBasic.PowerPacks.Printing.PrintForm(this.components);
+            this.tabControl1.SuspendLayout();
+            this.infoTab.SuspendLayout();
+            this.enfoTab.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "apple",
+            "orange",
+            "banana"});
+            this.checkedListBox1.Location = new System.Drawing.Point(33, 277);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(120, 94);
+            this.checkedListBox1.TabIndex = 0;
+            // 
+            // submitButton
+            // 
+            this.submitButton.Location = new System.Drawing.Point(228, 348);
+            this.submitButton.Name = "submitButton";
+            this.submitButton.Size = new System.Drawing.Size(75, 23);
+            this.submitButton.TabIndex = 1;
+            this.submitButton.Text = "Submit";
+            this.submitButton.UseVisualStyleBackColor = true;
+            this.submitButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.infoTab);
+            this.tabControl1.Controls.Add(this.enfoTab);
+            this.tabControl1.Location = new System.Drawing.Point(12, 27);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(370, 406);
+            this.tabControl1.TabIndex = 2;
+            // 
+            // infoTab
+            // 
+            this.infoTab.Controls.Add(this.print);
+            this.infoTab.Controls.Add(this.checkedListBox1);
+            this.infoTab.Controls.Add(this.submitButton);
+            this.infoTab.Location = new System.Drawing.Point(4, 22);
+            this.infoTab.Name = "infoTab";
+            this.infoTab.Padding = new System.Windows.Forms.Padding(3);
+            this.infoTab.Size = new System.Drawing.Size(362, 380);
+            this.infoTab.TabIndex = 0;
+            this.infoTab.Text = "Info";
+            this.infoTab.UseVisualStyleBackColor = true;
+            // 
+            // print
+            // 
+            this.print.Location = new System.Drawing.Point(228, 319);
+            this.print.Name = "print";
+            this.print.Size = new System.Drawing.Size(75, 23);
+            this.print.TabIndex = 2;
+            this.print.Text = "Print";
+            this.print.UseVisualStyleBackColor = true;
+            this.print.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // enfoTab
+            // 
+            this.enfoTab.Controls.Add(this.sendToClass);
+            this.enfoTab.Controls.Add(this.formTwoPass);
+            this.enfoTab.Controls.Add(this.richTextBox1);
+            this.enfoTab.Location = new System.Drawing.Point(4, 22);
+            this.enfoTab.Name = "enfoTab";
+            this.enfoTab.Padding = new System.Windows.Forms.Padding(3);
+            this.enfoTab.Size = new System.Drawing.Size(362, 380);
+            this.enfoTab.TabIndex = 1;
+            this.enfoTab.Text = "enfo";
+            this.enfoTab.UseVisualStyleBackColor = true;
+            // 
+            // sendToClass
+            // 
+            this.sendToClass.Location = new System.Drawing.Point(89, 306);
+            this.sendToClass.Name = "sendToClass";
+            this.sendToClass.Size = new System.Drawing.Size(75, 23);
+            this.sendToClass.TabIndex = 6;
+            this.sendToClass.Text = "Send to Class";
+            this.sendToClass.UseVisualStyleBackColor = true;
+            this.sendToClass.Click += new System.EventHandler(this.sendToClass_Click);
+            // 
+            // formTwoPass
+            // 
+            this.formTwoPass.Location = new System.Drawing.Point(194, 306);
+            this.formTwoPass.Name = "formTwoPass";
+            this.formTwoPass.Size = new System.Drawing.Size(75, 23);
+            this.formTwoPass.TabIndex = 5;
+            this.formTwoPass.Text = "Form2 Pass";
+            this.formTwoPass.UseVisualStyleBackColor = true;
+            this.formTwoPass.Click += new System.EventHandler(this.formTwoPass_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(71, 107);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(220, 167);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.viewToolStripMenuItem,
+            this.steupToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(397, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openFileToolStripMenuItem,
+            this.saveTextToolStripMenuItem,
+            this.openYourMindToolStripMenuItem});
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.openToolStripMenuItem.Text = "Open";
+            // 
+            // openFileToolStripMenuItem
+            // 
+            this.openFileToolStripMenuItem.Name = "openFileToolStripMenuItem";
+            this.openFileToolStripMenuItem.Size = new System.Drawing.Size(162, 22);
+            this.openFileToolStripMenuItem.Text = "Open File";
+            this.openFileToolStripMenuItem.Click += new System.EventHandler(this.openFileToolStripMenuItem_Click);
+            // 
+            // saveTextToolStripMenuItem
+            // 
+            this.saveTextToolStripMenuItem.Name = "saveTextToolStripMenuItem";
+            this.saveTextToolStripMenuItem.Size = new System.Drawing.Size(162, 22);
+            this.saveTextToolStripMenuItem.Text = "Save File";
+            this.saveTextToolStripMenuItem.Click += new System.EventHandler(this.saveTextToolStripMenuItem_Click);
+            // 
+            // openYourMindToolStripMenuItem
+            // 
+            this.openYourMindToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sToolStripMenuItem});
+            this.openYourMindToolStripMenuItem.Name = "openYourMindToolStripMenuItem";
+            this.openYourMindToolStripMenuItem.Size = new System.Drawing.Size(162, 22);
+            this.openYourMindToolStripMenuItem.Text = "Open Your Mind";
+            // 
+            // sToolStripMenuItem
+            // 
+            this.sToolStripMenuItem.Name = "sToolStripMenuItem";
+            this.sToolStripMenuItem.Size = new System.Drawing.Size(94, 22);
+            this.sToolStripMenuItem.Text = "60\'s";
+            this.sToolStripMenuItem.Click += new System.EventHandler(this.sToolStripMenuItem_Click);
+            // 
+            // viewToolStripMenuItem
+            // 
+            this.viewToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tab1ToolStripMenuItem,
+            this.tab2ToolStripMenuItem,
+            this.form2ToolStripMenuItem});
+            this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            this.viewToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.viewToolStripMenuItem.Text = "View";
+            // 
+            // tab1ToolStripMenuItem
+            // 
+            this.tab1ToolStripMenuItem.Name = "tab1ToolStripMenuItem";
+            this.tab1ToolStripMenuItem.Size = new System.Drawing.Size(111, 22);
+            this.tab1ToolStripMenuItem.Text = "Tab 1";
+            this.tab1ToolStripMenuItem.Click += new System.EventHandler(this.tab1ToolStripMenuItem_Click);
+            // 
+            // tab2ToolStripMenuItem
+            // 
+            this.tab2ToolStripMenuItem.Name = "tab2ToolStripMenuItem";
+            this.tab2ToolStripMenuItem.Size = new System.Drawing.Size(111, 22);
+            this.tab2ToolStripMenuItem.Text = "Tab 2";
+            this.tab2ToolStripMenuItem.Click += new System.EventHandler(this.tab2ToolStripMenuItem_Click);
+            // 
+            // form2ToolStripMenuItem
+            // 
+            this.form2ToolStripMenuItem.Name = "form2ToolStripMenuItem";
+            this.form2ToolStripMenuItem.Size = new System.Drawing.Size(111, 22);
+            this.form2ToolStripMenuItem.Text = "Form 2";
+            this.form2ToolStripMenuItem.Click += new System.EventHandler(this.form2ToolStripMenuItem_Click);
+            // 
+            // steupToolStripMenuItem
+            // 
+            this.steupToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fontToolStripMenuItem,
+            this.colorToolStripMenuItem});
+            this.steupToolStripMenuItem.Name = "steupToolStripMenuItem";
+            this.steupToolStripMenuItem.Size = new System.Drawing.Size(49, 20);
+            this.steupToolStripMenuItem.Text = "Steup";
+            // 
+            // fontToolStripMenuItem
+            // 
+            this.fontToolStripMenuItem.Name = "fontToolStripMenuItem";
+            this.fontToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.fontToolStripMenuItem.Text = "Font";
+            this.fontToolStripMenuItem.Click += new System.EventHandler(this.fontToolStripMenuItem_Click);
+            // 
+            // colorToolStripMenuItem
+            // 
+            this.colorToolStripMenuItem.Name = "colorToolStripMenuItem";
+            this.colorToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.colorToolStripMenuItem.Text = "Color";
+            this.colorToolStripMenuItem.Click += new System.EventHandler(this.colorToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(244, 439);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 4;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // printForm1
+            // 
+            this.printForm1.DocumentName = "document";
+            this.printForm1.Form = this;
+            this.printForm1.PrintAction = System.Drawing.Printing.PrintAction.PrintToPreview;
+            this.printForm1.PrinterSettings = ((System.Drawing.Printing.PrinterSettings)(resources.GetObject("printForm1.PrinterSettings")));
+            this.printForm1.PrintFileName = null;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(397, 472);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.infoTab.ResumeLayout(false);
+            this.enfoTab.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Button submitButton;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage infoTab;
+        private System.Windows.Forms.TabPage enfoTab;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveTextToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openYourMindToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem steupToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.ToolStripMenuItem tab1ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tab2ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem form2ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fontToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem colorToolStripMenuItem;
+        private System.Windows.Forms.Button print;
+        private Microsoft.VisualBasic.PowerPacks.Printing.PrintForm printForm1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button formTwoPass;
+        private System.Windows.Forms.Button sendToClass;
+    }
+}
+
---
+++ C#/Lab Samples/menus/menus/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace menus
+{
+    public partial class Form1 : Form
+    {
+        TextClass textClass = new TextClass();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            foreach(object element in checkedListBox1.CheckedItems){
+                MessageBox.Show(element.ToString());
+            }
+
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void tab1ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            tabControl1.SelectedIndex = 0;
+        }
+
+        private void tab2ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            tabControl1.SelectedIndex = 1;
+        }
+
+        private void form2ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            this.Hide();
+            form2.Show();
+
+        }
+
+        private void openFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    
+
+                            System.IO.StreamReader SReader = new System.IO.StreamReader(openFileDialog1.FileName);
+            richTextBox1.Text = SReader.ReadToEnd();
+     
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+
+        }
+
+        private void fontToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FontDialog fontDialog1 = new FontDialog();          
+            if (fontDialog1.ShowDialog() != DialogResult.Cancel){
+                exitButton.Font = fontDialog1.Font;
+
+            }
+
+        }
+
+        private void colorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ColorDialog colorDialog1 = new ColorDialog();
+            if (colorDialog1.ShowDialog() != DialogResult.Cancel)
+            {
+                submitButton.BackColor = colorDialog1.Color;
+
+            }
+        }
+
+        private void sToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hippie");
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            printForm1.Print();
+        }
+
+       
+         private void saveTextToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.InitialDirectory = "C:";
+            saveFileDialog1.Title = "Save File";
+            saveFileDialog1.FileName = " ";
+            saveFileDialog1.Filter = "Text Files|.txt|Word Document|.doc";
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox1.SaveFile(saveFileDialog1.FileName, RichTextBoxStreamType.PlainText);
+            }
+        }
+
+         private void sendToClass_Click(object sender, EventArgs e)
+         {
+             textClass.textString = richTextBox1.Text;
+             MessageBox.Show(textClass.textString);
+         }
+
+         private void formTwoPass_Click(object sender, EventArgs e)
+         {
+             Form2 form2 = new Form2();
+             form2.setClass(textClass);
+             this.Hide();
+             form2.Show();
+         }
+    }
+}
