479ce1e1b3815ad72750b459da03513403b6a822
Michael <ryuuko@eastlink.ca>
2014-11-10 13:07:22 -0400

    update

---
+++ C#/Class/StudentLibrary/StudentLibrary/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentLibrary
+{
+    public class Student
+    {
+        public String studentId { get; set; }
+        public String lastName { get; set; }
+        public String firstName { get; set; }
+        public String address { get; set; }
+        public String phoneNumber { get; set; }
+
+        public override String ToString()
+        {
+            String studentInfo = "Student Id:  " + studentId + "\r\nLast Name:  " + lastName + "\r\nFirst Name:  "
+                + firstName + "\r\nAddress:  " + address + "\r\nPhone Number:  " + phoneNumber;
+            return studentInfo;
+        }
+    }
+}
